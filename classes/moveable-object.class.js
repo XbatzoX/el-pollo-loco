d22994cace2d2c8acdb7ff869be4f9b06f2f8f35
BEN <benbasic@outlook.de>
2025-12-20 17:32:51 +0100

    added pepe to canvas

--- classes/moveable-object.class.js
+++ classes/moveable-object.class.js
@@ -1,7 +1,9 @@
 class MoveableObject{
-    position_x;
-    position_y;
+    position_x = 120;
+    position_y = 250;
     img;
+    width = 100;
+    height = 150;
 
     loadImage(path){
         this.img = new Image(); // document.getElementById('image') <img id="image">
