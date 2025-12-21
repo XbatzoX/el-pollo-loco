51600526713f3746bfed63a84fc020c60c762b98
BEN <benbasic@outlook.de>
2025-12-21 11:16:14 +0100

    added random position of chickens

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,5 +2,6 @@ class Chicken extends MoveableObject {
 
     constructor(){
         super().loadImage('../assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.position_x = 200 + (Math.random() * 500);
     }
 }
