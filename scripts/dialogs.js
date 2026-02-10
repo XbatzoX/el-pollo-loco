54fd8bd2f995e0e4039d20ded6d6bb39f3a3035b
BEN <benbasic@outlook.de>
2026-02-10 15:03:22 +0100

    added instructions dialog

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -7,3 +7,13 @@ function closeControlsDialog(){
     const contentDialogRef = document.getElementById('controls_dialog');
     contentDialogRef.close();
 }
+
+function openInstructionsDialog(){
+    const instructionsDialogRef = document.getElementById('instructions_dialog');
+    instructionsDialogRef.showModal(); 
+}
+
+function closeInstructionsDialog(){
+    const instructionsDialogRef = document.getElementById('instructions_dialog');
+    instructionsDialogRef.close();
+}
