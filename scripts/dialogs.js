c48db4eea0f094f2c117bc117d007919a34a975c
BEN <benbasic@outlook.de>
2026-02-10 12:44:36 +0100

    finished controls dialog

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -2,3 +2,8 @@ function openControlsDialog(){
     const contentDialogRef = document.getElementById('controls_dialog');
     contentDialogRef.showModal();
 }
+
+function closeControlsDialog(){
+    const contentDialogRef = document.getElementById('controls_dialog');
+    contentDialogRef.close();
+}
